3a59f5049ef1abdf24226c314d6baf8bd5c5f37e
Thai Hoang Van <thaihoangv@HarveyNash.vn.local>
2018-07-09 11:11:26 +0700

    update login

--- resources/views/includes/alert.blade.php
+++ resources/views/includes/alert.blade.php
@@ -1,18 +1,28 @@
-<br />
-<br />
-<br />
 <div class="row">
-@if (session('success'))
-<div class="alert alert-success alert-dismissable">
-    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-    <strong>Thành công!</strong> {{ session('success') }}
-</div>
+@if (session('success') || session('status'))
+	<div class="alert alert-success alert-dismissable">
+		@if (session('status'))
+			<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		    <strong>Thành công!</strong> {{ session('status') }}
+		@else
+		    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		    <strong>Thành công!</strong> {{ session('success') }}
+		@endif
+	</div>
 @endif
-@if (session('err'))
-<div class="alert alert-danger  alert-dismissable">
-    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-    <strong>Thất bại!</strong> {{ session('err') }}
-</div>
+@if (session('err') || session('errors'))
+	@if (session('errors'))
+		<ul>
+		    @foreach ($errors->all() as $error)
+		        <li style="color:red">{{ $error }}</li>
+		    @endforeach
+		</ul>
+	@else
+	<div class="alert alert-danger  alert-dismissable">
+	    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+	    <strong>Thất bại!</strong> {{ session('err') }}
+	</div>
+	@endif
 @endif
 @if (session('info'))
 <div class="alert alert-info alert-dismissable">
