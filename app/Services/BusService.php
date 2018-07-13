f2453db27090df1b9096a917aa74d10d514e253d
Kyvv <matkhautenem@gmail.com>
2018-07-13 21:02:52 +0700

    update bustype

--- app/Services/BusService.php
+++ app/Services/BusService.php
@@ -34,7 +34,8 @@ class BusService
     public function getJSONData($search = "")
     {
         return DataTables::of(
-            $this->busModel->where('id', '!=', 0)
+            $this->busModel->select('bus_name', 'bus_reg_number', 'bus_type_id', DB::raw('(select bus_type_name from bus_types where bus_types.id = buses.bus_type_id) AS bus_type'), 'number_seats', 'start_point', 'end_point', 'start_time', 'end_time')
+                ->where('id', '!=', 0)
         )
             ->filter(function ($query) use ($search) {
                 if(!empty($search))
--- resources/views/admin/bus/index.blade.php
+++ resources/views/admin/bus/index.blade.php
@@ -47,7 +47,7 @@
                 columns: [
                     { data: 'bus_name', name: 'bus_name', title: 'Bus Name' },
                     { data: 'bus_reg_number', name: 'bus_reg_number', title: 'Bus RegiNumber' },
-                    { data: 'bus_type_id', name: 'bus_type_id', title: 'Bus Type' },
+                    { data: 'bus_type', name: 'bus_type', title: 'Bus Type' },
                     { data: 'number_seats', name: 'number_seats', title: 'Number Seats' },
                     { data: 'start_point', name: 'start_point', title: 'Start Point' },
                     { data: 'end_point', name: 'end_point', title: 'End Point'},
