17bc063839807683d436a87d393eaa0bba2fbeaa
Kyvv <matkhautenem@gmail.com>
2018-07-15 18:00:02 +0700

    update

--- resources/views/admin/bus/index.blade.php
+++ resources/views/admin/bus/index.blade.php
@@ -21,7 +21,7 @@
             <div class="modal-content">
                 <div class="modal-header" style="padding:35px 50px;">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    Modal
+                    <h3 class="box-title">Thông tin chi tiết xe bus</h3>
                 </div>
                 <div class="modal-body" style="padding:40px 50px;">
                 </div>
