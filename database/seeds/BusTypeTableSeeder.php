217030d5e85ddb98229d5eaf33296d68cd1bfa15
Pham Van Dung <dungpv10@gmail.com>
2018-07-25 00:34:12 +0700

    fix seeder

--- database/seeds/BusTypeTableSeeder.php
+++ database/seeds/BusTypeTableSeeder.php
@@ -39,14 +39,16 @@ class BusTypeTableSeeder extends Seeder
                 factory(\App\Models\Bus::class, 5)->make()
             );
 
-
-            \App\Models\Bus::all()->each(function($bus) {
-                $bus->routes()->saveMany(factory(\App\Models\Route::class, 2)->make());
-                $bus->images()->saveMany(factory(\App\Models\BusImage::class, 2)->make());
-                $bus->seatLayout()->save(factory(\App\Models\SeatLayout::class)->make());
-
-                $bus->rates()->saveMany(factory(\App\Models\Rate::class, 2)->make());
-            });
         });
+
+        \App\Models\Bus::all()->each(function($bus) {
+            $bus->routes()->saveMany(factory(\App\Models\Route::class, 2)->make());
+            $bus->images()->saveMany(factory(\App\Models\BusImage::class, 2)->make());
+            $bus->seatLayout()->save(factory(\App\Models\SeatLayout::class)->make());
+
+            $bus->rates()->saveMany(factory(\App\Models\Rate::class, 2)->make());
+        });
+
+
     }
 }
