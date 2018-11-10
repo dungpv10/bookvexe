4f1eaa486f13c8ee64482a21e969f77e997d19de
kyvv <matkhautenem@gmail.com>
2018-11-10 13:45:06 +0700

    update rating

--- resources/views/admin/rating/index.blade.php
+++ resources/views/admin/rating/index.blade.php
@@ -1,33 +1,54 @@
-@extends('admin.layouts.dashboard')
+@extends('admin.layouts.master_layout')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-3">
-            <div class="form-group">
-                <select class="form-control select2" id="bus_id">
-                    <option value="">Chọn xe</option>
-                    @foreach($buses as $bus)
-                        <option value="{{ $bus->id }}">{{ $bus->bus_name }}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="box">
 
-                <div class="table-responsive">
-
-                    <table class="table table-bordered " id="rating_table">
-
-                    </table>
-
+    <div class="breadcomb-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="breadcomb-list">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    <select class="selectpicker" id="bus_id">
+                                        <option value="">Chọn xe</option>
+                                        @foreach($buses as $bus)
+                                            <option value="{{ $bus->id }}">{{ $bus->bus_name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
 
+    <div class="data-table-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
+                    <div class="data-table-list">
+                        <div class="basic-tb-hd">
+                            <h2>Danh sách đánh giá</h2>
+                            <p>
+                                Dưới đây là danh sách các đánh giá thuộc quyền quản lý của bạn
+                            </p>
+                        </div>
+                        <div class="table-responsive">
+
+                            <table class="table table-striped" id="rating_table">
+
+                            </table>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
 
 @stop
@@ -36,7 +57,7 @@
 @section('js')
     <script type="text/javascript">
         var ratingTable, busId = $('#bus_id');
-        $('.select2').select2();
+        // $('.select2').select2();
         $('.select2-container--default').css({width: '100%'});
         $(function(){
             ratingTable = $('#rating_table').DataTable({
