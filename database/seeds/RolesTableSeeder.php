e38248fc874044d2df8d50a309de88a12ddc47e5
Pham Van Dung <dungpv10@gmail.com>
2019-01-20 14:03:52 +0700

    add seeder

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -16,18 +16,22 @@ class RolesTableSeeder extends Seeder
             [
                 'name' => 'root',
                 'label' => 'Root',
+                'module_ids' => '0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15'
             ],
             [
                 'name' => 'admin',
                 'label' => 'Quản lý nhà xe',
+                'module_ids' => '0,1,2,3,4,5,6'
             ],
             [
                 'name' => 'staff',
                 'label' => 'Nhân viên',
+                'module_ids' => ''
             ],
             [
                 'name' => 'customer',
                 'label' => 'Khách hàng',
+                'module_ids' => ''
             ]
         ]);
     }
